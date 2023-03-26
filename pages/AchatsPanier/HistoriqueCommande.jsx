32783de81d5f3c4cf801928bb2a15800762639e7
MiguelJerome <sharkmaycry@gmail.com>
2023-03-26 06:39:40 -0400

    fix inventory stock

--- pages/AchatsPanier/HistoriqueCommande.jsx
+++ pages/AchatsPanier/HistoriqueCommande.jsx
@@ -10,16 +10,15 @@ import styles from '/styles/Cart.module.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default function HistoriqueCommande({
-  cart,
-  purchaseDate,
-  purchaseTime,
-  currentTime,
-  orders,
-  orderString,
-}) {
+export default function HistoriqueCommande({ purchaseDate, orders }) {
+  let userInfo;
+  if (typeof window !== 'undefined') {
+    userInfo = localStorage.getItem('token-info');
+  }
+  const { orderString } = userInfo ? JSON.parse(userInfo) : {};
+
   const [orderHistory, setOrderHistory] = useState(orders);
-  // let cart = [];
+  let cart = [];
 
   //console.log("Orders in string passing from the current cart", orders);
 
@@ -33,6 +32,7 @@ export default function HistoriqueCommande({
   // console.log("Cart in string ", cart);
 
   // console.log("Order History in string ", orderHistory);
+  //console.log('Order History in string ', orderString);
   return (
     <>
       <Header />
@@ -50,30 +50,15 @@ export async function getServerSideProps(context) {
   const { orders } = context.query;
   const currentDate = new Date().toLocaleDateString();
   const currentTime = new Date().toISOString();
-  let orderString = null;
 
   // If the orders parameter is present in the query string, parse it from a string back into an array.
   const ordersArray = orders ? JSON.parse(orders) : [];
-
-  // If the orderString is present in the localStorage, parse it from a string and push it to the orders array.
-  if (typeof window !== 'undefined') {
-    const userInfo = localStorage.getItem('token-info');
-    if (userInfo) {
-      const { orderString: parsedOrderString } = JSON.parse(userInfo);
-      if (parsedOrderString) {
-        orderString = parsedOrderString;
-        const parsedOrders = JSON.parse(orderString);
-        ordersArray.push(...parsedOrders);
-      }
-    }
-  }
 
   return {
     props: {
       purchaseDate: currentDate,
       currentTime: currentTime,
       orders: ordersArray || [],
-      orderString: orderString,
     },
   };
 }
