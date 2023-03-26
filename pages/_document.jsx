94930e1f7e550d7b58694c0810149ec2a6a9eadc
MiguelJerome <sharkmaycry@gmail.com>
2023-03-26 15:33:11 -0400

    fix bug production

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,10 +1,28 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { DefaultSeo } from 'next-seo';
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <DefaultSeo
+            title="Animago - Accueil"
+            description="Une page d'accueil de site web, avec une image de Animago propriétaire du e-commerce"
+            openGraph={{
+              title: 'Animago - Accueil',
+              description:
+                "Une page d'accueil de site web, avec une image de Animago propriétaire du e-commerce",
+              images: [
+                {
+                  url: 'http://localhost:3000/img/AnimagoLogo.png',
+                  alt: 'Animago',
+                },
+              ],
+              site_name: 'Animago',
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
@@ -13,4 +31,3 @@ export default class MyDocument extends Document {
     );
   }
 }
-//
