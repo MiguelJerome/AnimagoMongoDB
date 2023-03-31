c9fa4a4ceb34e7a29bec4ca405bb00ea5b330baf
MiguelJerome <sharkmaycry@gmail.com>
2023-03-31 14:49:33 -0400

    fix production

--- server/config/connexion.cjs
+++ server/config/connexion.cjs
@@ -2,10 +2,13 @@ const mongoose = require('mongoose');
 
 mongoose.set('strictQuery', false);
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/animago',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 const db = mongoose.connection;
 
