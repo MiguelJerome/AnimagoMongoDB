c0a61e2bc79f9323affc506969793fbcbeed1a31
MiguelJerome <sharkmaycry@gmail.com>
2023-03-31 12:27:11 -0400

    fix production

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,10 +2,12 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Head from 'next/head';
 import { getPaniersProps } from '/components/ServerProps/getPaniersProps';
+import RedirectToAccueil from '../components/RedirectToAccueil';
 
 export default function Home({ panier }) {
   return (
     <>
+      <RedirectToAccueil />
       <Head>
         <title>Animago - Accueil</title>
       </Head>
