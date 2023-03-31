9e2fd22d9b1b8ad6f9819f05d086197aba66d2fd
MiguelJerome <sharkmaycry@gmail.com>
2023-03-31 12:59:11 -0400

    first commit

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -5,13 +5,6 @@ import '@/styles/globals.css';
 import Head from 'next/head';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
-import ImagePrincipal from '../public/img/image_accueil.png';
-import facebook from '../public/img/facebook.svg';
-import instagram from '../public/img/instagram.svg';
-import youtube from '../public/img/youtube.svg';
-import search from '../public/img/search.png';
-import Login from '../public/img/login.png';
-import Cart from '../public/img/cart.png';
 
 export default function App({ Component, pageProps }) {
   return (
